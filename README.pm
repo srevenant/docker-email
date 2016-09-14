248e36001247f10135a6ef336c7164cf8ab95d2c
s3tqr2w <brandon.gillespie@kuali.co>
2016-09-14 13:24:02 -0600

    adding README.pm

---
+++ README.pm
@@ -0,0 +1,9 @@
+Full service secured/spam resistant SMTP server, with IMAP and SASL support as well.  Includes Spamassassin and Greylisting, with a local volume import for all of the secured configurations and email folders.
+
+Components:
+
+* Postfix (SMTP / SMTPS w/SASL)
+* Dovecot (IMAPS)
+* Spamassassin
+* Greylisting
+* rsyslog (cause postfix and others want to send to syslog, boo)
